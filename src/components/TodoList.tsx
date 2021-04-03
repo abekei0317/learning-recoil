cf52a7beae564d00101375130c58cfa1b0ad568a
阿部圭一郎 <keiichiroabe@abekeiichirounoMacBook-Air.local>
2021-04-03 22:48:05 +0900

    feat: add filter and stats

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,14 +2,16 @@ import { useRecoilValue } from "recoil";
 import { todoListState } from "./../state/todoList";
 import TodoItemCreator from "./TodoItemCreator";
 import TodoItem from "./TodoItem";
+import TodoListStats from "./TodoListStats";
+import TodoListFilters from "./TodoListFilters";
 
 const TodoList: React.FC = () => {
   const todoList = useRecoilValue(todoListState);
 
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem) => (
